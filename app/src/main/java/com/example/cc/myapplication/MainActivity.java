ab1025ba10023899a2335be5cc58a903621347f1
chun.chen <chun.chen@wonhe.net>
2017-07-12 10:53:33 +0800

    初始化导入

---
+++ app/src/main/java/com/example/cc/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.cc.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import pl.droidsonroids.gif.GifImageView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        GifImageView testgif = (GifImageView) findViewById(R.id.testgif);
+        testgif.setImageResource(R.mipmap.giftest);
+
+    }
+}
